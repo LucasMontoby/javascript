9941c4c86d6a594f93a456cc0f39f64a1d1dd120
Lucas <montobybianchi@gmail.com>
2022-09-29 14:02:27 -0300

    javascript UTN

---
+++ practica1.js
@@ -0,0 +1,39 @@
+// let edad = 39;
+// let peso = 99;
+
+// let holaQueTal = "Hola mundo"
+
+// console.log(holaQueTal)
+
+// let verdadero = true;
+// let nada = undefined;
+// let texto = "Lucas";
+// let numero = 39;
+
+// let positivo = Number(39)
+// let negativo = Number(-39)
+// let decimal = Number(39.5)
+
+// let nan =parseInt()
+// let nan =parseInt(135.25)
+
+// let primerNumero = 2;
+// let segundoNumero = 2;
+
+
+// let resultadoSuma = primerNumero + segundoNumero;
+// let resta = primerNumero - segundoNumero;
+
+// console.log(resultadoSuma)
+
+// function ejemplo(nombre, apellido){
+//     return "hola" + nombre + '' + apellido
+// };
+
+// function esPar(numero){
+// if(numero % 2 == 0){
+//     return true}
+//     return false
+// }
+
+// console.log(numero(11))
---
+++ practica2.js
@@ -0,0 +1,80 @@
+// let dato = true;
+// if (dato){
+// console.log("es true")
+// }
+// else{
+//     console.log ("es false")
+// }
+
+// let alumnos = ["juan", "francisco", "jorge", "pepe"]
+// let indiceJuan = alumnos.indexOf("juan")
+// let indiceFrancisco = alumnos.indexOf("francisco")
+
+// console.log(indiceFrancisco)
+
+//------------------------------
+
+// let estudiantes = [
+//     {
+//         nombre: "Lucas",
+//         promedio: 9,
+//         curso: "Full stack"
+//     },
+//     {
+//         nombre: "Anto",
+//         promedio: 10,
+//         curso: "Android"
+//     },
+//     {
+//         nombre: "Alexis",
+//         promedio: 8,
+//         curso: "ios"
+//     },
+// ];
+
+// let alumnoEgresado = estudiantes.pop(0)
+
+// console.log(alumnoEgresado)
+
+//-----------------------------------
+
+// let estudiantes = [
+//         {
+//             nombre: "Lucas",
+//             promedio: 9,
+//             curso: "Full stack"
+//         },
+//         {
+//             nombre: "Anto",
+//             promedio: 10,
+//             curso: "Android"
+//         },
+//         {
+//             nombre: "Alexis",
+//             promedio: 8,
+//             curso: "ios"
+//         },
+//     ];
+
+//     estudiantes.push(
+//         {
+//             nombre: "henry",
+//             promedio: 7,
+//             curso: "UTN"
+//         },
+//     )
+
+    
+
+//     let alumnoBaja = estudiantes.shift(0)
+
+
+//     estudiantes.unshift(
+//         {
+//             nombre: "Anto",
+//             promedio: 10,
+//             curso: "Android" 
+//         }
+//     )
+
+//     console.log(estudiantes);
