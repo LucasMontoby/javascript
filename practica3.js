061b334769c4a2f449717707da43ae34fffd0bc1
Lucas <montobybianchi@gmail.com>
2022-10-04 10:31:38 -0300

    js

---
+++ practica3.js
@@ -0,0 +1,58 @@
+//Metodos
+
+// function dominio (string){
+//     return ("http://www." + string)
+// }
+
+// console.log(dominio("digitalhouse.com.ar"))
+
+// let texto = ('string');
+
+// console.log(texto.length);
+
+// function reemplazoFastFast (texto, palabra1, palabra2){
+//     return texto.replace(palabra1, palabra2)
+// };
+// console.log(reemplazoFastFast("estas", "estan" ))
+
+// function menciona (texto, palabra){
+//     if(texto.indexOf(palabra)== -1){
+//         return "ksksks" ==false
+//     };
+//         return true
+// }
+
+// console.log(menciona)
+
+// let frase = 'Hola!, soy Carli';
+// let licenciada = 'Carli'
+
+// console.log(frase.slice(11))
+
+//Objetos literales
+
+// let perro = {
+//     nombre: "Pedro",
+//     edad: 5,
+//     vacunado: true
+
+// }
+
+// console.log(perro)
+// let contador = 5
+// let deportista = {
+//     nombre: "Messi",
+//     energia: 100,
+//     experiencia: 10,
+//     entrenarHoras: function (cantidadHoras){
+//         // console.log("hola")
+//         this.energia = this.energia -(cantidadHoras * 5);
+//         this.experiencia = this.experiencia + (cantidadHoras *2);
+// }
+// }
+
+// console.log(deportista.entrenarHoras(contador));
+// console.log(deportista.experiencia)
+// console.log(deportista.energia)
+// console.log(deportista)
+
---
+++ practica4.js
@@ -0,0 +1,42 @@
+// let suma = (numero1, numero2) => numero1 + numero2;
+// let resta = (numero1, numero2) => numero1 - numero2;
+// let multiplicacion = (numero1, numero2) => numero1 * numero2;
+// let division = (numero1, numero2) => numero1 / numero2;
+
+const { __esModule } = require("babel-preset-react");
+
+// let calculadora = (numero1, numero2, operacion) => operacion (numero1, numero2);
+
+// console.log(calculadora(25, 50, multiplicacion));
+
+// let estudiantes = [
+//     {nombre: 'John', promedio: 8.5, aprobado: true},
+//     {nombre: 'Jane', promedio: 7, aprobado: true},
+//     {nombre: 'June', promedio: 3, aprobado: false},
+// ]
+
+// let aprobados = estudiantes.filter(estudiantes => estudiantes.aprobado == true); 
+
+// let desaprobados = estudiantes.filter(estudiantes => estudiantes.aprobado == false);
+
+
+// console.log(desaprobados)
+
+// let listaDeSuperMercado = [
+//     'Bife de chorizo', 
+//     'Coca Cola', 
+//     'Bananas', 
+//     'Lechuga', 
+//     'Chimichurri', 
+//     'Lata de tomates', 
+//     'Arvejas', 
+//     'Cereales', 
+//     'Pechuga de pollo', 
+//     'Leche'
+// ];
+
+// listaDeSuperMercado.forEach(function(alimentos){
+//     console.log(alimentos)
+// });
+
+
